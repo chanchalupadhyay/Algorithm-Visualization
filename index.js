f0d776cb2060be6f528b2839caa5dcb3154f2740
verma <chanchalverma.48@gmail.com>
2020-04-08 17:12:44 +0530

    update css

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let inputDataArray = [];
 const algoContainer = document.querySelector(".algo-animation-container");
-
 //  for generate Blocks
+
 function generateBlocks() {
-  let len = Number(document.getElementById("input-size").value);
+  let length = document.getElementById("input-size").value;
+  let len = length || 10;
+
   let value;
   for (let i = 0; i < len; i++) {
     value = Math.floor(Math.random() * 100);
+
     const block = document.createElement("div");
     block.classList.add("block");
     block.style.height = `${value * 1}px`;
@@ -23,15 +25,13 @@ function generateBlocks() {
     block.appendChild(blockLabel1);
     algoContainer.appendChild(block);
   }
+
   document.getElementById("input-search-value").disabled = false;
 }
 
 function reset() {
-  // document.getElementById("input-value").value = "";
-  // document.getElementById("error").innerHTML = "";
   document.getElementById("input-size").value = "";
   document.getElementById("input-search-value").value = "";
-  inputDataArray.length = 0;
   let inputArray = document.querySelectorAll(".display-values");
   let inputArray1 = document.querySelectorAll(".algo-animation-container");
 
@@ -43,3 +43,4 @@ function reset() {
     input.innerHTML = "";
   });
 }
+generateBlocks();
