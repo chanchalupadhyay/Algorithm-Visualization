f9aa19b6288781f8e502564fdad84bd9dd63592a
verma <chanchalverma.48@gmail.com>
2020-04-29 15:36:59 +0530

    updated

--- index.js
+++ index.js
@@ -1,45 +1,160 @@
+const RED_COLOR = "#FF4949";
+const BLUE_COLOR = "#58B7FF";
+const GREEN_COLOR = "#13CE66";
 const algoContainer = document.querySelector(".algo-animation-container");
-//  for generate Blocks
 
-function generateBlocks() {
+let inputArray = [];
+function generateRandomArray() {
   let length = document.getElementById("input-size").value;
   let len = length || 10;
+  let randomArray;
 
-  let value;
   for (let i = 0; i < len; i++) {
-    value = Math.floor(Math.random() * 100);
+    {
+      randomArray = Math.floor(Math.random() * 100);
+      inputArray.push(randomArray);
+    }
+  }
+}
+
+//  for generate Blocks
+function generateBlocks() {
+  // remove previous array
+  let child = algoContainer.lastElementChild;
+  while (child) {
+    algoContainer.removeChild(child);
+    child = algoContainer.lastElementChild;
+  }
+  inputArray = [];
+  generateRandomArray();
+
+  let lengthOfArray = inputArray.length;
+  for (let i = 0; i < lengthOfArray; i++) {
     const block = document.createElement("div");
     block.classList.add("block");
-    block.style.height = `${value * 1}px`;
-    if (typeof value == "string") {
-      block.style.height = `${Math.floor(value.charCodeAt(0) / 2)}px`;
-    }
-
+    block.style.height = `${inputArray[i] * 1}px`;
     block.style.transform = `translateX(${i * 30}px)`;
-
     const blockLabel1 = document.createElement("label");
-
     blockLabel1.classList.add("block_id");
-    blockLabel1.innerHTML = value;
+    blockLabel1.innerHTML = inputArray[i];
     block.appendChild(blockLabel1);
     algoContainer.appendChild(block);
   }
-
   document.getElementById("input-search-value").disabled = false;
 }
 
 function reset() {
-  document.getElementById("input-size").value = "";
-  document.getElementById("input-search-value").value = "";
-  let inputArray = document.querySelectorAll(".display-values");
-  let inputArray1 = document.querySelectorAll(".algo-animation-container");
+  window.location.reload();
+}
 
-  inputArray.forEach(function(input) {
-    input.innerHTML = "";
-  });
-  inputArray1.forEach(function(input) {
-    input.value = "";
-    input.innerHTML = "";
+//all algoritmn
+async function algorithmn(choosenAlgo) {
+  const length = inputArray.length;
+  let blocks = document.querySelectorAll(".block");
+
+  switch (choosenAlgo) {
+    case "bubbleSort":
+      let swapped;
+      do {
+        swapped = false;
+        for (let j = 0; j < length - 1; j++) {
+          blocks[j].style.backgroundColor = RED_COLOR;
+          blocks[j + 1].style.backgroundColor = RED_COLOR;
+
+          await delay();
+
+          if (inputArray[j] > inputArray[j + 1]) {
+            let temp = inputArray[j];
+            let tempNum = blocks[j].innerHTML;
+            let tempHeight = blocks[j].style.height;
+
+            inputArray[j] = inputArray[j + 1];
+            blocks[j].innerHTML = blocks[j + 1].innerHTML;
+            blocks[j].style.height = blocks[j + 1].style.height;
+            inputArray[j + 1] = temp;
+            blocks[j + 1].innerHTML = tempNum;
+            blocks[j + 1].style.height = tempHeight;
+
+            swapped = true;
+          }
+
+          blocks[j].style.backgroundColor = BLUE_COLOR;
+          blocks[j + 1].style.backgroundColor = BLUE_COLOR;
+        }
+      } while (swapped);
+      console.log(inputArray);
+      break;
+
+    case "selectionSort":
+      for (let i = 0; i < length; i++) {
+        let minIndex = i;
+
+        for (let j = i + 1; j < length; j++) {
+          blocks[minIndex].style.backgroundColor = RED_COLOR;
+          blocks[j].style.backgroundColor = RED_COLOR;
+          await delay();
+
+          if (inputArray[minIndex] > inputArray[j]) {
+            blocks[minIndex].style.backgroundColor = BLUE_COLOR;
+            minIndex = j;
+          } else blocks[j].style.backgroundColor = BLUE_COLOR;
+        }
+        if (minIndex != i) {
+          let temp = inputArray[minIndex];
+
+          let tempNum = blocks[minIndex].innerHTML;
+          let tempHeight = blocks[minIndex].style.height;
+
+          inputArray[minIndex] = inputArray[i];
+          blocks[minIndex].innerHTML = blocks[i].innerHTML;
+          blocks[minIndex].style.height = blocks[i].style.height;
+
+          inputArray[i] = temp;
+          blocks[i].innerHTML = tempNum;
+          blocks[i].style.height = tempHeight;
+        }
+        blocks[minIndex].style.backgroundColor = BLUE_COLOR;
+        blocks[i].style.backgroundColor = BLUE_COLOR;
+      }
+
+      break;
+    case "insertionSort":
+      let i, key, j;
+
+      for (i = 1; i < length; i++) {
+        key = inputArray[i];
+        let keyheight = blocks[i].style.height;
+        let keytext = blocks[i].innerHTML;
+        j = i - 1;
+
+        await delay();
+        while (j >= 0 && inputArray[j] > key) {
+          inputArray[j + 1] = inputArray[j];
+
+          blocks[j + 1].innerHTML = blocks[j].innerHTML;
+          blocks[j + 1].style.height = blocks[j].style.height;
+          j = j - 1;
+        }
+        inputArray[j + 1] = key;
+        blocks[j + 1].innerHTML = keytext;
+        blocks[j + 1].style.height = keyheight;
+      }
+      break;
+    case "linearSearch":
+      searchAlgo(choosenAlgo);
+
+      break;
+    case "binarySearch":
+      searchAlgo(choosenAlgo);
+
+      break;
+  }
+}
+
+function delay() {
+  return new Promise((resolve) => {
+    setInterval(() => {
+      resolve();
+    }, 250);
   });
 }
-generateBlocks();
--- search.js
+++ search.js
@@ -1,24 +1,68 @@
-var count = 0; // record times of press searching button if more than one not allowed to create again search box animation
+async function searchAlgo(choosenAlgo) {
+  let length = inputArray.length;
+  let searchValue = document.getElementById("input-search-value").value;
 
-function search() {
-  let input = document.getElementById("input-search-value").value;
-  console.log(input);
+  let blocks = document.querySelectorAll(".block");
 
-  if (!input) {
-    document.getElementById("input-search-value").style.backgroundColor = "red";
-  } else if (count == 0) {
-    document.getElementById("input-search-value").style.backgroundColor =
-      "white";
+  switch (choosenAlgo) {
+    case "linearSearch":
+      for (let i = 0; i < blocks.length; i++) {
+        blocks[i].style.backgroundColor = RED_COLOR;
+        await delay();
+        if (inputArray[i] == searchValue) {
+          blocks[i].style.backgroundColor = GREEN_COLOR;
 
-    let searchBox = document.createElement("div");
-    searchBox.classList.add("searchBox");
-    searchBox.style.border = "1px solid #58B7FF";
+          return;
+        }
+        blocks[i].style.backgroundColor = BLUE_COLOR;
+      }
+      break;
+    case "binarySearch":
+      inputArray.sort((a, b) => a - b);
+      // remove previous unsorted array
+      let child = algoContainer.lastElementChild;
+      while (child) {
+        algoContainer.removeChild(child);
+        child = algoContainer.lastElementChild;
+      }
+      // create sorted array
+      for (let i = 0; i < length; i++) {
+        const block = document.createElement("div");
+        block.classList.add("block");
+        block.style.height = `${inputArray[i] * 1}px`;
+        block.style.transform = `translateX(${i * 30}px)`;
+        const blockLabel1 = document.createElement("label");
+        blockLabel1.classList.add("block_id");
+        blockLabel1.innerHTML = inputArray[i];
+        block.appendChild(blockLabel1);
+        algoContainer.appendChild(block);
+      }
 
-    let label = document.createElement("label");
-    label.classList.add("search-box-value");
-    label.textContent = input;
-    searchBox.appendChild(label);
-    algoContainer.appendChild(searchBox);
-    count++;
+      blocks = document.querySelectorAll(".block");
+      searchContainer = document.querySelectorAll(".searchBox");
+      let lowIndex = 0;
+      let highIndex = length;
+      let midIndex;
+      while (lowIndex <= highIndex) {
+        midIndex = Math.floor((lowIndex + highIndex) / 2);
+        console.log("ss" + midIndex);
+        blocks[lowIndex].style.backgroundColor = RED_COLOR;
+
+        await delay();
+        if (inputArray[midIndex] == searchValue) {
+          blocks[midIndex].style.backgroundColor = GREEN_COLOR;
+          console.log("found");
+          return;
+        } else if (inputArray[midIndex] > searchValue) {
+          blocks[lowIndex].style.backgroundColor = BLUE_COLOR;
+          highIndex = midIndex - 1;
+        } else {
+          blocks[lowIndex].style.backgroundColor = BLUE_COLOR;
+          lowIndex = midIndex + 1;
+        }
+      }
+      alert("not found");
+      return null;
+      break;
   }
 }
